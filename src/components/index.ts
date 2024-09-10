e11cf6f70529d983129b24640cef67b96ec896de
himalkarkee000 <himalkarkee000@gmail.com>
2024-09-10 12:10:47 +0545

    Not finished but working

---
+++ src/components/index.ts
@@ -0,0 +1,23 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+
+import Navbar from "./Navbar";
+import Footer from "./footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetail from "./CarDetail";
+
+export {
+    Hero,
+    CustomButton,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    Navbar,
+    Footer,
+    CarCard,
+    CarDetail
+    
+}
